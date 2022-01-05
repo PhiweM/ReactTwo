79c2ad9321b0d754d81d977253207978b4fdb8bf
Phiwe <wycliffwhite7@gmail.com>
2022-01-04 21:34:47 -0500

    FIles before revert

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
-
-
-  function RenderCampsite({campsite}) {
-    return (
-      <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  }
-
-  function RenderComments({comments}) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => {
-            return (
-              <div>
-                <p>
-                  {comment.text} <br />
-                  -- {comment.author},{" "}
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  }).format(new Date(Date.parse(comment.date)))}
-                </p>
-              </div>
-=======
 import { Component } from 'react';
 import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
 
@@ -79,24 +40,9 @@ class CampsiteInfo extends Component {
                     }
                     )}
                 </div>
->>>>>>> parent of 69f6e52 (added comments)
             );
         }
 
-<<<<<<< HEAD
-  function CampsiteInfo(props) {
-    if (props.campsite) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.campsite.comments} />
-                </div>
-            </div>
-        );
-    }
-    return <div />;
-=======
         return <div />
     };
 
@@ -115,7 +61,6 @@ class CampsiteInfo extends Component {
              );
          }
     }
->>>>>>> parent of 69f6e52 (added comments)
 }
 
 export default CampsiteInfo;
