fc4fdf37a7761ba951613d6a032c4f084fb67890
Phiwe <wycliffwhite7@gmail.com>
2022-01-04 21:32:03 -0500

    reverted state

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
@@ -35,14 +36,54 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                   }).format(new Date(Date.parse(comment.date)))}
                 </p>
               </div>
-            );
-          })}
-        </div>
-      );
-    }
-    return <div />;
-  };
+=======
+import { Component } from 'react';
+import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
 
+class CampsiteInfo extends Component {
+    
+    renderCampsite(campsite) {
+        return(
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    
+    renderComments (comments){
+        if(comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comment) => {
+                        return (
+                            <div>
+                                <p>
+                                    {comment.text} <br />
+                                      {comment.author}, {" "}
+                                      {new Intl.DateTimeFormat("en-US", {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "2-digit",
+                                        }).format(new Date(Date.parse(comment.date)))}
+                                </p>
+                            </div>
+                        );
+                    }
+                    )}
+                </div>
+>>>>>>> parent of 69f6e52 (added comments)
+            );
+        }
+
+<<<<<<< HEAD
   function CampsiteInfo(props) {
     if (props.campsite) {
         return (
@@ -55,6 +96,26 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         );
     }
     return <div />;
+=======
+        return <div />
+    };
+
+    render() {
+        if(this.props.campsite){
+            return(
+                <div className="row" >
+                    {this.renderCampsite(this.props.campsite)}{" "}
+                    {this.renderComments(this.props.comments)}
+                </div>
+            );
+        }
+         else {
+             return(
+                 <div></div>
+             );
+         }
+    }
+>>>>>>> parent of 69f6e52 (added comments)
 }
 
 export default CampsiteInfo;
