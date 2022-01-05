7f99d82579df6e6b204f6355b112624a4bb4f756
Phiwe <wycliffwhite7@gmail.com>
2022-01-04 21:23:30 -0500

    reset back to no comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-  renderCampsite(campsite) {
+
+
+  function RenderCampsite({campsite}) {
     return (
       <div className="col-md-5 m-1">
         <Card>
@@ -16,7 +17,7 @@ class CampsiteInfo extends Component {
     );
   }
 
-  renderComments = (comments) => {
+  function RenderComments({comments}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -42,18 +43,18 @@ class CampsiteInfo extends Component {
     return <div />;
   };
 
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div className="row">
-          {this.renderCampsite(this.props.campsite)}{" "}
-          {this.renderComments(this.props.campsite.comments)}
-        </div>
-      );
-    } else {
-      return <div></div>;
+  function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
     }
-  }
+    return <div />;
 }
 
 export default CampsiteInfo;
